4b467e86e8670358f79b78755c5b3194a4795b3c
Tamas Palotas <tamas.palotas@etu.univ-cotedazur.fr>
2025-11-19 15:33:01 +0100

    feat: add script for tagging and pushing Docker images

---
+++ docker_tag_and_push.cmd
@@ -0,0 +1,4 @@
+docker login
+docker tag bigdata-sandbox:latest shiyamiii/bigdata-sandbox:v1
+docker tag bigdata-sandbox:latest shiyamiii/bigdata-sandbox:lastest
+docker push shiyamiii/bigdata-sandbox:v1
